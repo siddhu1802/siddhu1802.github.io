a06d8ee63168ed9843a8cab0a80ba32e6e6482f2
siddhu1802 <kssiddhu18@gmail.com>
2025-06-06 16:27:03 +0530

    done

--- p2/script.js
+++ p2/script.js
@@ -1,10 +1,12 @@
+let users = [];
+
 const showLogin = () => {
     let str=`
     <div class='App-Container'>
     <h1>Login Form</h1>
-    <p><input type="text" id="txtEmail"></p>
-    <p><input type="password" id="txtPass"></p>
-    <p><button>Log In</button></p>
+    <p><input type="text" id="txtEmail" placeholder="email"></p>
+    <p><input type="password" id="txtPass" placeholder="password"></p>
+    <p><button onclick='loginUser()'>Log In</button></p>
     <p><button onclick='showRegister()'>Create Account</button></p>
     </div>
     `
@@ -12,19 +14,46 @@ const showLogin = () => {
 }
 
 const showRegister = () => {
-      let str=`
+    let str=`
+    <div class='App-Container'>
     <h1>Register Form</h1>
-    <p><input type="text" id="txtName"></p>
-     <p><input type="text" id="txtEmail"></p>
-    <p><input type="password" id="txtPass"></p>
-    <button>Register</button>
+    <p><input type="text" id="txtName" placeholder="name"></p>
+     <p><input type="text" id="txtEmail" placeholder="email"></p>
+    <p><input type="password" id="txtPass" placeholder="password"></p>
+    <button onclick='registerUser()'>Register</button>
+    <button onclick='showLogin()'>already have an account</button>
+    </div>
     `
     root.innerHTML = str
 }
 
+function registerUser() {
+    const name = document.getElementById('txtName').value;
+    const email = document.getElementById('txtEmail').value;
+    const pass = document.getElementById('txtPass').value;
+    if(name && email && pass) {
+        users.push({ name, email, pass });
+        alert('Registration successful! Please login.');
+        showLogin();
+    } else {
+        alert('Please fill all fields');
+    }
+}
+
+function loginUser() {
+    const email = document.getElementById('txtEmail').value;
+    const pass = document.getElementById('txtPass').value;
+    const user = users.find(u => u.email === email && u.pass === pass);
+    if(user) {
+        showHome();
+    } else {
+        alert('Invalid email or password');
+    }
+}
+
 const showHome = () => {
     let str=`
-    <div class='App-home'>
+    <div class='App-Container'>
     <h1>Welcome to the Home Page</h1>
     <button onclick='showLogin()'>Log Out</button>
     </div>
